0313dc4db009f71dc9c704ef78714fb0aae3de09
Rhythm Saha <rs.2001.saha@gmail.com>
2024-03-02 11:45:39 +0530

    Add toast notifications and update default form values

--- sections/products/EditProductSection.tsx
+++ sections/products/EditProductSection.tsx
@@ -5,6 +5,8 @@ import { useForm } from 'react-hook-form';
 import { IProductFormData } from './NewProductSection';
 import useSingleProduct from '@/hooks/products/useSingleProduct';
 import { useRouter } from 'next/router';
+import toast from 'react-hot-toast';
+import ProductForm from '@/components/products/ProductForm';
 
 const EditProductSection: FC = () => {
     const [images, setImages] = useState<ImageFileType[]>([]);
@@ -13,26 +15,50 @@ const EditProductSection: FC = () => {
 
     const { product, productLoading } = useSingleProduct(router.query.id as string);
 
-    console.log(product);
-
     const {
         handleSubmit,
         control,
         setValue,
         getValues,
         formState: { errors, isSubmitting },
-    } = useForm<IProductFormData>({});
+    } = useForm<IProductFormData>({
+        defaultValues: {
+            published: true,
+            specs: [{ name: 'Product Category', value: 'Saree' }],
+        },
+    });
 
     useEffect(() => {
         getCategories(true);
     }, [getCategories, setValue]);
 
+    useEffect(() => {
+        const _cat = categories.map((c, i) => {
+            const _field = getValues('attributes')[i];
+            if (_field?.subcategory) return _field;
+
+            return {
+                _id: c._id,
+                category: c.name,
+            };
+        });
+
+        setValue('attributes', _cat);
+    }, [categories, setValue, getValues]);
+
+    const submitHandler = async (data: IProductFormData) => {
+        if (isSubmitting) return;
+
+        try {
+            toast.success('Product Updated Successfully!');
+        } catch (error: any) {
+            toast.error(error.message);
+        }
+    };
+
     return (
-        <section className="mt-10 grid grid-cols-2 gap-4">
-            <div>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur eaque nulla aut odit, enim eius eos rerum illum quas laborum, reiciendis consequuntur harum ex quod asperiores
-                omnis, similique necessitatibus unde.
-            </div>
+        <section className="mt-10">
+            <ProductForm control={control} images={images} setImages={setImages} categories={categories} onSubmit={handleSubmit(submitHandler)} />
         </section>
     );
 };
